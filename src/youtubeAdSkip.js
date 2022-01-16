a9013bbb0329b9682f504c9b5dfee3819e18105d
supia99 <kazuking317zeus@yahoo.co.jp>
2022-01-16 22:30:27 +0900

    feat: 下に出る広告画像を消す

--- src/youtubeAdSkip.js
+++ src/youtubeAdSkip.js
@@ -10,6 +10,7 @@ setInterval(() => {
   // time = time + TIME_INTERVAL
   // console.log('youtube経過時間: ', time);
 
+  // 広告をskipする
   skipButton = document.getElementsByClassName('ytp-ad-skip-button-text');
   if(skipButton.length !== 0) {
     // console.log('skip buttonあり: ', skipButton);
@@ -18,11 +19,18 @@ setInterval(() => {
     // console.log('skip button無し:', skipButton);
   }
 
+  // 「広告までO秒」を非表示にする
   adMessageContainer = document.getElementsByClassName('ytp-ad-message-container');
   if(adMessageContainer.length !== 0) {
     adMessageContainer[0].style.display = 'none';
   } else {
     // console.log('adMessageContainer無し')
+  }
+
+  // 下に出てくる広告画像を消す
+  adPictureXButton = document.getElementsByClassName('ytp-ad-overlay-close-button');
+  if(adPictureXButton.length !== 0) {
+    adPictureXButton[0].click();
   }
 
 }, TIME_INTERVAL);
